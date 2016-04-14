b408697cdce3fd3c2629c4d66b36061a2fa5a45e
PrasanthReddyYerradoddi <prasanth.reddy.93@gmail.com>
2016-04-13 19:19:18 -0700

    start

--- chat.c
+++ chat.c
@@ -10,7 +10,5 @@ int main( int argc, char *argv[] )  {
    else if( argc > 2 ) {
       printf("Too many arguments supplied.\n");
    }
-   else {
-      printf("One argument expected.\n");
-   }
+  
 }
