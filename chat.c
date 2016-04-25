a606e1b5bd35339b46b1c07e74034747ade78c92
PrasanthReddyYerradoddi <prasanth.reddy.93@gmail.com>
2016-04-25 14:15:13 -0700

    connect

--- chat.c
+++ chat.c
@@ -12,6 +12,70 @@
 #include <arpa/inet.h>
 #include <ifaddrs.h>
 
+void server(){
+  int serverSocket,client_connected,len;
+  struct sockaddr_in client_addr,server_addr;
+  struct hostent *ptrh;
+  struct sockaddr_in sin;
+  int n=0; 
+  char message[100],received[100];
+
+  serverSocket=socket(AF_INET, SOCK_STREAM, 0);
+
+  memset((char*)&server_addr,0,sizeof(server_addr));
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(sin.sin_port);
+
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if(bind(serverSocket,
+  (struct sockaddr*)&server_addr,sizeof(server_addr)) == -1)
+  printf("Bind Failure\n");
+  else
+  printf("Bind Success:<%u>\n", serverSocket);
+  
+
+
+
+
+  while(1)
+  {   
+       listen(serverSocket,5);
+       len=sizeof(struct sockaddr_in);
+
+      client_connected=accept(serverSocket,
+      (struct sockaddr*)&client_addr,&len);
+  if (-1 != client_connected)
+    printf("Connection accepted:<%u>\n", client_connected);
+
+      while(1)
+      {
+      n = read(client_connected, received, sizeof(received));
+      if( (strcmp(received,"q") == 0 ) || (strcmp(received,"Q") == 0 ))
+      {
+         printf("Wrong place...Socket Closed of Client\n");
+         close(client_connected);
+         break;
+      }
+      else{
+      printf("\nUser:-%s", received);}
+      printf("\nServer:-");
+    //  memset(message, '\0', 10);
+      gets(message);             
+      write(client_connected, message, sizeof(message));
+      if( (strcmp(message,"q") == 0 ) || (strcmp(message,"Q") == 0 ))
+      {
+         printf("Wrong place...Socket Closed of Client\n");
+         close(client_connected);
+         break;
+      }  
+      }
+  }
+
+  close(serverSocket); printf("\nServer Socket Closed !!\n");
+}
+
 void help(){
 	printf("connect <destination> <port no>:\t Establishes a new TCP connection to the specified <destination> at the specified < port no>.\n\n");
 	printf("terminate <connection id>:\t\t Will terminate the connection listed under the specified number when LIST is used to display all connections.\n\n");
@@ -23,29 +87,87 @@ void help(){
 	
 }
 
-void myip(){
-	int n;
-    struct ifreq ifr;
-    char array[] = "en1";
+// void myip(){
+// 	int n;
+//     struct ifreq ifr;
+//     char array[] = "en1";
  
-    n = socket(AF_INET, SOCK_STREAM, 0);
-    //Type of address to retrieve - IPv4 IP address
-    ifr.ifr_addr.sa_family = AF_INET;
-    //Copy the interface name in the ifreq structure
-    strncpy(ifr.ifr_name , array , IFNAMSIZ - 1);
-    ioctl(n, SIOCGIFADDR, &ifr);
-    close(n);
-    //display result
-    printf("IP Address is %s\n" , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr) );
-}
+//     n = socket(AF_INET, SOCK_STREAM, 0);
+//     //Type of address to retrieve - IPv4 IP address
+//     ifr.ifr_addr.sa_family = AF_INET;
+//     //Copy the interface name in the ifreq structure
+//     strncpy(ifr.ifr_name , array , IFNAMSIZ - 1);
+//     ioctl(n, SIOCGIFADDR, &ifr);
+//     close(n);
+//     //display result
+//     printf("IP Address is %s\n" , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr) );
+// }
 
 void myport(){
 
-   struct sockaddr_in sin;
-socklen_t len = sizeof(sin);
-    printf("port number %d\n", ntohs(sin.sin_port));
+    struct sockaddr_in sin;
+    printf("port number %d\n", htons(sin.sin_port));
 
 }
+
+void client(int p1){
+    int clientSocket; /* Socket Decriptor for Client */
+    struct sockaddr_in server_addr;
+    struct hostent *ptrh;
+
+    char message[100];
+    char received[100];
+    int n = 0;
+
+    clientSocket=socket(AF_INET, SOCK_STREAM, 0);
+
+    memset((char*)&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(p1);
+
+    /*  bind(clientSocket, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)); */
+
+    ptrh=gethostbyname("10.85.109.255");
+    memcpy(&server_addr.sin_addr,ptrh->h_addr,ptrh->h_length);
+
+    if( -1 == (connect(clientSocket, (struct sockaddr*)&server_addr, sizeof(server_addr)))) 
+    { printf("\nServer Not Ready !!\n"); exit(1); }
+
+while(1)
+{
+    printf("\nUser:-");
+   // memset(message, '\0', 10);
+
+    gets(message);
+
+    n = write(clientSocket, message, strlen(message)+1);
+if( (strcmp(message,"q") == 0 ) || (strcmp(message,"Q") == 0 ))
+    {
+       printf("Wrong place...Socket Closed\n");
+       close(clientSocket);
+       break;
+    }
+
+    //printf("Write:<%u>\n", n);
+
+    read(clientSocket, received, sizeof(received));
+    if( (strcmp(received,"q") == 0 ) || (strcmp(received,"Q") == 0 ))
+    {
+       printf("Wrong place...Socket Closed\n");
+       close(clientSocket);
+       break;
+    }
+    else
+    printf("Server:- %s\n", received);
+
+
+
+}
+
+}
+
+
 
 
 int main( int argc, char *argv[] )  {
@@ -60,6 +182,7 @@ int main( int argc, char *argv[] )  {
   }
   else{
    printf("Type 'help' for more information\n");
+   // printf(argv[1] );
    
    struct ifreq ifr;
     char array[] = "en1";
@@ -72,7 +195,7 @@ int main( int argc, char *argv[] )  {
     ioctl(n, SIOCGIFADDR, &ifr);
    while(1){
    	char command[100];
-   	scanf("%s",command);
+   	scanf("%[^\n]s",command);
 
    	if(strcmp(command, "exit") == 0){
    		exit(0);
@@ -81,14 +204,23 @@ int main( int argc, char *argv[] )  {
    		help();
    	}
    	else if(strcmp(command, "myip") == 0){
-   		myip();
+      printf("IP Address is %s\n" , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr) );
+   		//myip();
    	}
    	else if(strcmp(command, "myport") == 0){
    		myport();
    	}
-   	// else if(strcmp(command, "connect") == 0){
-   	// 	connecting();	
-   	// }
+   	else if(strcmp(command, "connect") == 0){
+      char s[50];
+      strcpy(s,command);
+      char* p1,p2;
+      p1 = strtok(s," ");
+      p1 = strtok(NULL, " ");
+      p2 = strtok(NULL, " ");
+      client(p1);
+     
+   			
+   	}
    	// else if(strcmp(command, "list") == 0){
    	// 	list();
    	// }
